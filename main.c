3adcf324a86734232b715477e7cc9272af122706
GolfBravoSierra <106974254+GolfBravoSierra@users.noreply.github.com>
2023-10-05 13:24:59 -0300

    05/10/23 - caso 2 WorkInProgress

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <time.h>
 
+void selecionaMenu(); // protótipo da função
+
 void menu(){
     printf("1. Adicionar uma nova tarefa\n");
     printf("2. Modificar uma tarefa\n");
@@ -76,9 +78,57 @@ No* ins_fim(No* fim, Tarefa A){
     fim->prox=p;
     return p;
 }
+/*
+//função para modificar uma tarefa
+Tarefa modificatarefa(Tarefa tarefa , int codigotarefa){
+    int flag = 0;
+    while(flag != 1){
+        if(tarefa.codigoTarefa == codigotarefa){
+            printf("\nDigite o nome da tarefa: ");
+            scanf("%s", &tarefa.nomeTarefa);
+            printf("\nDigite o nome do projeto: ");
+            scanf("%s", &tarefa.nomeProjeto);
+            printf("\ndigite a data de inicio: (dia/mes/ano):\n");
+            do{
+                printf("\nDigite o dia: ");
+                scanf("%d", &tarefa.dataInicio_dia);
+                }while(tarefa.dataInicio_dia > 31 || tarefa.dataInicio_dia < 1);
+            do{
+                printf("\nDigite o mes: ");
+                scanf("%d", &tarefa.dataInicio_mes);
+                }while(tarefa.dataInicio_mes > 12 || tarefa.dataInicio_mes < 1);
+                printf("\nDigite o ano: ");
+                scanf("%d", &tarefa.dataInicio_ano);
+            printf("\nData de inicio da tarefa: %d/%d/%d", tarefa.dataInicio_dia, tarefa.dataInicio_mes, tarefa.dataInicio_ano);
+
+            printf("\ndigite a data de termino: (dia/mes/ano)\n");
+            do{
+                printf("\nDigite o dia: ");
+                scanf("%d", &tarefa.dataTermino_dia);
+                }while(tarefa.dataTermino_dia > 31 || tarefa.dataTermino_dia < 1);
+            do{
+                printf("\nDigite o mes: ");
+                scanf("%d", &tarefa.dataTermino_mes);
+                }while(tarefa.dataTermino_mes > 12 || tarefa.dataTermino_mes < 1);
+                printf("\nDigite o ano: ");
+            scanf("%d", &tarefa.dataTermino_ano);
+            printf("\nData de termino da tarefa: %d/%d/%d", tarefa.dataTermino_dia, tarefa.dataTermino_mes, tarefa.dataTermino_ano);
+
+            flag = 1;
+            return tarefa;
+        }
+        else{
+            
+
+            flag = 1;
+        }
+    }
 
 
-
+    return tarefa;
+}
+*/
+//função para criar uma nova tarefa
 Tarefa novatarefa(){
     Tarefa tarefa;
     printf("\nDigite o codigo da tarefa: ");
@@ -120,7 +170,7 @@ void insereFila(Fila* fila, Tarefa tarefa){
     fila->fim=ins_fim(fila->fim,tarefa);
     if(fila->ini==NULL)/*fila antes vazia?*/
     fila->ini=fila->fim;
-    menu;
+    menu();
 };
 
 void selecionaMenu(){
@@ -129,13 +179,16 @@ void selecionaMenu(){
     scanf("%d", &opcao);
     system("cls");
     Tarefa tarefa;
+    int codigo;
     switch(opcao){
         case 1:
             printf("Adicionar uma nova tarefa\n");
             tarefa = novatarefa();
             insereFila(fila , tarefa);
         case 2:
-            printf("Modificar uma tarefa\n");
+            printf("Modificar uma tarefa\nDigite o codigo da tarefa: ");
+            scanf("%d", &codigo);
+            tarefa = modificatarefa(tarefa, codigo);
             break;
         case 3:
             printf("Concluir uma tarefa\n");
