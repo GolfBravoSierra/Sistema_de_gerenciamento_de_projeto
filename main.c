52b9b1e94c8662a46d8cb8225a92859c98066f61
GolfBravoSierra <giovanigiovani.correio@gmail.com>
2023-10-08 16:27:26 -0300

    8/10/23 - caso 4 (working progress [caso 3])

--- main.c
+++ main.c
@@ -68,6 +68,12 @@ int vazia (Lista *recebida);
 // protótipo para função de inserir na lista encadeada
 Lista* insere (Lista* recebida, Tarefa valor);
 
+// protótipo para função que atualiza estatus de todas as tarefas
+void AtualizaStatus(Fila *fila);
+
+// protótipo para função que seta pendencia
+Lista* SetaPendencia(Fila *fila, int codigotarefa, Lista *lista);
+
 /// Função cria fila
 Fila *criaFila(){
     Fila *fila = (Fila*) malloc(sizeof(Fila));
@@ -88,9 +94,20 @@ No* ins_fim(No* fim, Tarefa A){
 }
 
 //função para modificar uma tarefa
-Tarefa modificatarefa (int codigotarefa){
-   // int flag = 0;
+//basicamente cria uma tarefa nova e insere ela na fila no lugar da que o usuario deseja mudar
+Tarefa modificatarefa (int codigotarefa, Fila *fila){
     Tarefa tarefa;
+
+    //imprimiando a tarefa que o usuario deseja mudar
+    printf("\nDados da Tarefa Escolhida:\n");
+    No *aux = fila->ini;
+    while(aux != NULL){
+        if(aux->tarefa.codigoTarefa == codigotarefa){
+            imprimeTarefa(aux->tarefa);
+        }
+        aux = aux->prox;
+    }
+
     tarefa.codigoTarefa = codigotarefa;
     while (getchar() != '\n');
     printf("\nDigite o nome da tarefa: ");
@@ -108,7 +125,8 @@ Tarefa modificatarefa (int codigotarefa){
         }while(tarefa.dataInicio.mes > 12 || tarefa.dataInicio.mes < 1);
         printf("\nDigite o ano: ");
         scanf("%d", &tarefa.dataInicio.ano);
-    printf("\nData de inicio da tarefa: %d/%d/%d", tarefa.dataInicio.ano, tarefa.dataInicio.ano, tarefa.dataInicio.ano);
+    printf("ignore a data errada abaixo... no final ela fica certa >;D ");
+    printf("\nComfirmacao da data de inicio da tarefa: %d/%d/%d", tarefa.dataInicio.ano, tarefa.dataInicio.ano, tarefa.dataInicio.ano);
     
     printf("\ndigite a data de termino: (dia/mes/ano)\n");
     do{
@@ -121,7 +139,7 @@ Tarefa modificatarefa (int codigotarefa){
         }while(tarefa.dataTermino.mes > 12 || tarefa.dataTermino.mes < 1);
         printf("\nDigite o ano: ");
     scanf("%d", &tarefa.dataTermino.ano);
-    printf("\nData de termino da tarefa: %d/%d/%d \n", tarefa.dataTermino.dia, tarefa.dataTermino.mes, tarefa.dataTermino.ano);
+    printf("\nComfirmacao da data de termino da tarefa: %d/%d/%d \n", tarefa.dataTermino.dia, tarefa.dataTermino.mes, tarefa.dataTermino.ano);
     tarefa.status = -1;
     
     return tarefa;
@@ -148,7 +166,7 @@ Tarefa novatarefa(){
         }while(tarefa.dataInicio.mes > 12 || tarefa.dataInicio.mes < 1);
         printf("\nDigite o ano: ");
         scanf("%d", &tarefa.dataInicio.ano);
-    printf("\nData de inicio da tarefa: %d/%d/%d", tarefa.dataInicio.dia, tarefa.dataInicio.mes, tarefa.dataInicio.ano);
+    printf("\nComfirmacoa da data de inicio da tarefa: %d/%d/%d", tarefa.dataInicio.dia, tarefa.dataInicio.mes, tarefa.dataInicio.ano);
     
     printf("\ndigite a data de termino: (dia/mes/ano)\n");
     do{
@@ -161,7 +179,7 @@ Tarefa novatarefa(){
         }while(tarefa.dataTermino.mes > 12 || tarefa.dataTermino.mes < 1);
         printf("\nDigite o ano: ");
     scanf("%d", &tarefa.dataTermino.ano);
-    printf("\nData de termino da tarefa: %d/%d/%d \n", tarefa.dataTermino.dia, tarefa.dataTermino.mes, tarefa.dataTermino.ano);
+    printf("\nComfirmacao da data de termino da tarefa: %d/%d/%d \n", tarefa.dataTermino.dia, tarefa.dataTermino.mes, tarefa.dataTermino.ano);
     tarefa.status = -1;
     return tarefa;
 };
@@ -174,7 +192,7 @@ void insereFila(Fila* fila, Tarefa tarefa){
 };
 
 
-/// Busca o codigo na fila
+/// Busca o codigo na fila e inseri a nova tarefa no lugar da antiga
 Fila *trocatarefa(Fila *fila , int codigotarefa , Tarefa tarefa){
 
     No *aux = fila->ini;
@@ -213,9 +231,21 @@ int main(){
     Tarefa tarefa;
     int codigo;
 
+    //lista de tarefas concluidas
     Lista *lista;
     lista = inicializaLista();
-    
+
+    //Lista de tarefas pendentes
+    Lista *listaPendente;
+    listaPendente = inicializaLista();
+
+    //variaveis para o caso 4
+    int resposta = 0;
+    int CodMod = 0;
+    int CodRet = 0;
+
+    system("cls");
+
 while(i == 0){
     menu();
     int opcao = 0;
@@ -227,6 +257,8 @@ while(i == 0){
             printf("\nAdicionar uma nova tarefa\n");
             tarefa = novatarefa();
             insereFila(fila , tarefa);
+            system("cls");
+            printf("listas com a nova tarefa:\n");
             imprimeFila(fila);
             break;
         case 2:
@@ -234,33 +266,61 @@ while(i == 0){
         porem dando ao usuario a oportunidade de trocar todos os dados da tarefa; depois o programa chama uma 
         função a qual pega a nova tarefa criada e seu código com isso ela procura na fila o código e caso ache 
         ele coloca a nova tarefa na posição da com o mesmo código */
-            printf("Modificar uma tarefa\nDigite o codigo da tarefa: ");
+            printf("Modificar uma tarefa\nDigite o codigo da tarefa que deseja modificar: ");
             scanf("%d", &codigo);
-            tarefa = modificatarefa(codigo);
+            tarefa = modificatarefa(codigo , fila);
             trocatarefa( fila, codigo , tarefa);
             codigo = 0;
+            system("cls");
+            printf("listas com a tarefa modificada:\n");
             imprimeFila(fila);
             break;
         case 3:
             printf("Concluir a primeira tarefa;\n");
             lista = ConcluirTarefa(fila , lista);
-            printf("Lista de tarefas concluidas:\n");
-            imprimeLista(lista);
             break;
         case 4:
             printf("Atualizacao do status da tarefa\n");
+            printf("Oque deseja fazer?\n1 -> Atualizar status de todas as tarefas: \n2 -> Colocar terafa como pendente: \n3 -> Retirar pendenci:\n4 -> Retornar ao menu:\n");
+            scanf("%d", &resposta);
+            switch (resposta)
+            {
+            case 1:
+                AtualizaStatus(fila);
+                printf("Status atualizado com sucesso\n");
+                break;
+            case 2:
+                printf("Digite o codigo da tarefa que deseja colocar na lista de pendentes: ");
+                scanf("%d", &CodMod);
+                printf("comfirmacao do codigo: %d\n", CodMod);
+                listaPendente = SetaPendencia(fila, CodMod, listaPendente);
+                break;
+            case 3:
+                printf("Digite o codigo da tarefa que deseja recolocar na fila: ");
+                scanf("%d", &CodRet);
+                //Tarefa tarefaaux;
+                //tarefaaux = RetiraPendencia(listaPendente, CodRet);
+                //insereFila(fila, tarefaaux);
+            case 4:
+                break;
+            default:
+                break;
+            }
             break;
         case 5:
             printf("Listar tarefas pendentes\n");
+            imprimeLista(listaPendente);
             break;
         case 6:
             printf("Listar tarefas concluidas\n");
+            imprimeLista(lista);
             break;
         case 7:
             printf("Listar tarefas concluidas com e sem atraso\n");
             break;
         case 8:
-            printf("Sair do programa\n");
+            printf("Sair do programa (imprime fila provisorio)\n");
+            imprimeFila(fila);
             break;
         default:
             printf("Opcao invalida\n");
@@ -283,7 +343,7 @@ No *ret_ini(No* ini){
 // função para concluir uma tarefa
 Lista *ConcluirTarefa(Fila *fila, Lista *lista){
 
-    //retirada da primeira Tkarefa da fila e colaca ela em um variavel auxiliar(tarefa)
+    //retirada da primeira Tarefa da fila e colaca ela em um variavel auxiliar(tarefa)
     Tarefa tarefa;
     int FlagDia = 0, FlagMes = 0, FlagAno = 0;
     Data SistemData;
@@ -293,6 +353,8 @@ Lista *ConcluirTarefa(Fila *fila, Lista *lista){
     fila->fim=NULL;
     printf("Tarefa %d conclida com sucesso\n", tarefa.codigoTarefa);
 
+    //verifica se a tarefa foi concluida no dia certo ou se foi concluida com atraso
+    //para definir o status da tarefa
     SistemData = recebeData();
 
     /// Dia
@@ -337,6 +399,7 @@ Lista *ConcluirTarefa(Fila *fila, Lista *lista){
 
 // função de imprimir menu
 void menu(){
+
     printf("1. Adicionar uma nova tarefa\n");
     printf("2. Modificar uma tarefa\n");
     printf("3. Concluir uma tarefa\n");
@@ -344,7 +407,7 @@ void menu(){
     printf("5. Listar tarefas pendentes\n");
     printf("6. Listar tarefas concluidas\n");
     printf("7. Listar tarefas concluidas com e sem atraso\n");
-    printf("8. Sair do programa\n");
+    printf("8. Sair do programa(imprime fila provisorio)\n");
     
 }
 
@@ -410,4 +473,65 @@ Lista* insere (Lista* recebida, Tarefa valor)
     novo->tarefa = valor;
     novo->prox = recebida;
     return novo;;
+}
+
+// função para Atualiuzar status de todas as tarefas
+void AtualizaStatus(Fila *fila){
+    No *aux = fila->ini;
+    Data dataSistem = recebeData();
+    int FlagDia = 0, FlagMes = 0, FlagAno = 0;  
+    while(aux != NULL){
+        FlagDia = 0;
+        FlagMes = 0;
+        FlagAno = 0;
+        if(aux->tarefa.dataTermino.dia < dataSistem.dia){
+            FlagDia = 1; 
+        }
+        else if(aux->tarefa.dataTermino.dia == dataSistem.dia){
+            FlagDia = 0;
+        }
+        if(aux->tarefa.dataTermino.mes < dataSistem.mes){
+            FlagMes = 1;
+        }
+        else if(aux->tarefa.dataTermino.mes == dataSistem.mes){
+            FlagMes = 0;
+        }
+        if(aux->tarefa.dataTermino.ano < dataSistem.ano){
+            FlagAno = 1;
+        }
+        else if(aux->tarefa.dataTermino.ano == dataSistem.ano){
+            FlagAno = 0;
+        }
+        if(FlagDia == 0 && FlagMes == 0 && FlagAno == 0){
+            aux->tarefa.status = 0;
+        }
+        else{
+            aux->tarefa.status = 1;
+        }
+        aux = aux->prox;
+    }
+}
+
+// função para setar pendencia
+Lista* SetaPendencia(Fila *fila, int codigotarefa, Lista *listaPendente){
+    Tarefa tarefaaux1 , tarefaaux2;
+    Fila *filaaux = criaFila();
+    No *aux = fila->ini;
+    while(aux != NULL){
+        if(aux->tarefa.codigoTarefa == codigotarefa){
+            tarefaaux1 = aux -> tarefa;
+            tarefaaux1.status = -1;
+            listaPendente = insere(listaPendente, tarefaaux1);
+            printf("lista de pendentes\n");
+            imprimeLista(listaPendente);
+        }
+        else{
+            tarefaaux2 = aux -> tarefa;
+            insereFila(filaaux, tarefaaux2);
+        }
+        aux = aux->prox;
+    }
+    fila-> ini = filaaux -> ini;
+    fila-> fim = filaaux -> fim;
+    return listaPendente;
 }
